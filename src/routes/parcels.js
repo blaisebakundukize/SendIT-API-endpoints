f910c5fa855727756b5d8902a1832a8f8d51bd35
blaisebakundukize <blaisebakundukize@gmail.com>
2018-11-08 12:00:31 +0200

    [finishes #161807591] Get a specific parcel delivery order

--- src/routes/parcels.js
+++ src/routes/parcels.js
@@ -14,4 +14,23 @@ router.get('/', (req, res) => {
   });
 });
 
+// Get a specific parcels
+router.get('/:parcelId', (req, res) => {
+  const parcelDelivery = Parcels.filter(
+    parcel => parcel.parcel.parcelId === req.params.parcelId
+  );
+
+  if (parcelDelivery.length > 0) {
+    return res.status(200).json({
+      success: true,
+      message: `Parcel of id ${req.params.parcelId} retrieved successfully`,
+      parcel: parcelDelivery[0]
+    });
+  }
+  return res.status(404).json({
+    success: false,
+    message: `Parcel of id ${req.params.parcelId} does not exist`
+  });
+});
+
 export default router;
